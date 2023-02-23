dba6ae3475343434ccad1b89a45891e153c7e65b
AJ Barea <79944464+ajbarea@users.noreply.github.com>
2023-02-23 16:47:21 -0500

    Update linkedListToText.c

--- P2-FileIO/linkedListToText.c
+++ P2-FileIO/linkedListToText.c
@@ -1,19 +1,17 @@
-Here is a possible C program that prints out a linked list to a text file:
-
 // A simple C program to print out a linked list to a text file
 #include <stdio.h>
 #include <stdlib.h>
 
 // A node of the linked list
-struct Node {
+struct node {
   int data; // Data field
-  struct Node* next; // Pointer to the next node
+  struct node* next; // Pointer to the next node
 };
 
 // A function to create a new node with a given data
-struct Node* newNode(int data) {
+struct node* newNode(int data) {
   // Allocate memory for the node
-  struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+  struct node* node = (struct node*)malloc(sizeof(struct node));
   // Check if the memory allocation was successful
   if (node == NULL) {
     printf("Memory allocation failed\n");
@@ -27,7 +25,7 @@ struct Node* newNode(int data) {
 }
 
 // A function to print out a linked list to a text file
-void printList(struct Node* head, char* filename) {
+void printList(struct node* head, char* filename) {
   // Open the file in write mode
   FILE* file = fopen(filename, "w");
   // Check if the file opening was successful
@@ -36,7 +34,7 @@ void printList(struct Node* head, char* filename) {
     exit(1);
   }
   // Traverse the linked list and print each node's data to the file
-  struct Node* temp = head;
+  struct node* temp = head;
   while (temp != NULL) {
     fprintf(file, "%d\n", temp->data);
     temp = temp->next;
@@ -46,25 +44,31 @@ void printList(struct Node* head, char* filename) {
 }
 
 // A function to free the memory allocated for the linked list
-void freeList(struct Node* head) {
-  // Traverse the linked list and free each node
-  struct Node* temp = head;
-  while (temp != NULL) {
-    struct Node* next = temp->next;
-    free(temp);
-    temp = next;
+void freeList(struct node *head) {
+  // Check if the linked list is empty
+  if (head == NULL) {
+    return;
+  } else {
+    // Traverse the linked list and free the memory of each node
+    struct node *current = head;
+    struct node *next;
+    while (current != NULL) {
+      next = current->next;
+      free(current);
+      current = next;
+    }
   }
 }
 
 // A main function to test the program
 int main() {
   // Create a sample linked list
-  struct Node* head = newNode(10);
+  struct node* head = newNode(10);
   head->next = newNode(20);
   head->next->next = newNode(30);
   head->next->next->next = newNode(40);
   // Print the linked list to a text file
-  printList(head, "list.txt");
+  printList(head, "data.txt");
   // Free the memory allocated for the linked list
   freeList(head);
   // Return 0 to indicate successful execution
