f050c026f5b1c4da7244821f7f4ed10b859db546
AJ Barea <79944464+ajbarea@users.noreply.github.com>
2023-02-23 16:52:49 -0500

    Update createFruitTxt.c

--- P3-Fruit/createFruitTxt.c
+++ P3-Fruit/createFruitTxt.c
@@ -1,10 +1,17 @@
+/*
+This program creates a file named fruit.txt and writes the details of five fruits in 
+comma-separated values (csv) format. The details include the name, color, and weight 
+of each fruit. The program checks for errors during opening and writing the file and 
+returns -1 if any error occurs.
+*/
+
 #include <stdio.h>
 
-// number of fruits to be written to fruit.txt
+// define a constant for the number of fruits
 #define FRUIT_COUNT 5
 
 int main() {
-	FILE* file;
+	FILE* file; // declare a file pointer
 
 	// open fruit.txt in write mode and check success
 	file = fopen("fruit.txt", "w");
@@ -13,16 +20,16 @@ int main() {
 		return -1;
 	}
 	
-	// arrays for fruit names, colors, and weights (grams)
+	// declare arrays for fruit names, colors, and weights (in grams)
 	char* fruits[] = {"apple", "banana", "orange", "grape", "strawberry"};
 	char* colors[] = {"red", "yellow", "orange", "purple", "red"};
 	int weights[] = {100, 120, 150, 5, 10};
 
-	// populate fruits.txt in csv format using array details
+	// write the fruit details to fruit.txt in csv format
 	for(int i = 0; i < FRUIT_COUNT; i++) {
 		fprintf(file, "%s,%s,%d\n", fruits[i], colors[i], weights[i]);
     	
-		// check for errors during file write
+		// check for errors during writing
 		if(ferror(file)){
 			printf(">> ERROR: file write fprintf");
 			return -1;
